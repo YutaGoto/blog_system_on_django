effc886b40f61bb43e55d467c4db4f590e8a3ad7
YutaGoto <you.goto.510@gmail.com>
2017-06-02 19:27:59 +0900

    many to many

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, PostsTag, Tag
+
+class PostsTagInline(admin.TabularInline):
+    model = PostsTag
+    extra = 1
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [PostsTagInline]
+    list_display = ('title', '_tags')
+
+    def _tags(self, post):
+        return ','.join([tag.name for tag in post.tags.all()])
+
+class TagAdmin(admin.ModelAdmin):
+    inlines = [PostsTagInline]
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag, TagAdmin)
--- blogs/models.py
+++ blogs/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 
-class Post(models.Model):
-    title = models.CharField(max_length = 30)
-    body = models.TextField(max_length = 1000)
-
 class Tag(models.Model):
     name = models.CharField(max_length = 30)
 
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length = 30)
+    body = models.TextField(max_length = 1000)
+    tags = models.ManyToManyField(Tag, through='PostsTag')
+
+    def __str__(self):
+        return self.title
+
 class PostsTag(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post)
+    tag = models.ForeignKey(Tag)
